7e02836e87476d6f65db849113be1a8c81ff8903
Subhodeep Bhattacharjee <hi.subho1994@gmail.com>
2023-07-17 14:18:19 +0530

    Create meeting

--- app/video-calling/[meetingid]/page.jsx
+++ app/video-calling/[meetingid]/page.jsx
@@ -1,34 +1,180 @@
 "use client";
-import VideoCalling from "@components/VideoCalling";
-import { useEffect, useState } from 'react';
+// import VideoCalling from "@components/VideoCalling";
+// import { useEffect, useState } from 'react';
+// import AgoraRTC from 'agora-rtc-sdk-ng';
+// import { useSession } from 'next-auth/react';
+
+// const JoinMeeting = () => {
+
+//   const [meetingId, setMeetingId] = useState('');
+//   const [client, setClient] = useState(null);
+//   const { data: session } = useSession();
+
+//   useEffect(() => {
+//     // Get the meetingId from the URL
+//     const url = window.location.href;
+//     const meetingId = url.split('/').pop();
+//     setMeetingId(meetingId);
+
+//     const agoraClient = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+//     setClient(agoraClient);
+
+//     // Join the meeting using the meetingId
+//     console.log('Joining meeting:', meetingId);
+
+//     // Implement your video calling logic here using the Agora SDK
+
+//     // Remember to clean up the video call when the component unmounts
+//     // return () => {
+//     //   // Clean up the video call
+//     // };
+//   }, []);
+
+//   // meetingId, username
+//   const joinAgadoMeeting = async () => {
+//     await client.join('b6b121e15e504a808e7092382509aab1', meetingId, null, session?.user.id);
+//     // continue with other logic to open the video calling box
+//   };
+
+//   return (
+//     <section className="w-full flex-center flex-col">
+//       <h1 className="head_text text-center">
+//           Discober & Shear
+//         <br className="max-md:hidden"/>
+//         <span className="orange_gradient text-center"> AI-Powred Prompts </span>
+//         <p className="desc text-center">
+//           Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts 
+//         </p>
+
+//         <div className=''>
+//           <button className='px-5 py-1.5 text-sm bg-orange-600 rounded-full text-white' onClick={joinAgadoMeeting}>Join Meeting</button>
+//         </div>
+//       </h1>
+//     </section>
+//   );
+// };
+
+// export default JoinMeeting;
+
+// "use client";
+// import { useState } from 'react';
+
+// const JoinMeetingComponent = () => {
+  
+
+  // useEffect(() => {
+  //   const agoraClient = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+  //   setClient(agoraClient);
+  // }, []);
+
+//   const joinMeeting = async (meetingId, username) => {
+//     await client.join('b6b121e15e504a808e7092382509aab1', meetingId, null, username);
+//     // continue with other logic to open the video calling box
+//   };
+
+//   return (
+//     <div>
+//       // your component JSX here
+//     </div>
+//   );
+// };
+
+// export default JoinMeetingComponent;
+
+
+import React, { useEffect, useRef } from 'react';
+import AgoraRTC from 'agora-rtc-sdk';
 
 const JoinMeeting = () => {
+  const client = useRef(null);
+  const localStream = useRef(null);
+  const remoteStream = useRef(null);
+  const remoteVideo = useRef(null);
 
-  const [meetingId, setMeetingId] = useState('');
   useEffect(() => {
-    // Get the meetingId from the URL
-    const url = window.location.href;
-    const meetingId = url.split('/').pop();
-    setMeetingId(meetingId);
+    const appId = 'b6b121e15e504a808e7092382509aab1';
+    const token = null; // Set to null if not using token
+    const channelName = 'MedicalApp_307047';
 
-    // Join the meeting using the meetingId
-    console.log('Joining meeting:', meetingId);
+    // Initialize Agora client
+    client.current = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+    client.current.init(appId, () => {
+      console.log('AgoraRTC client initialized');
+      client.current.join(token, channelName, null, (uid) => {
+        console.log('User ' + uid + ' joined channel ' + channelName);
+        // Create local stream
+        localStream.current = AgoraRTC.createStream({
+          streamID: uid,
+          audio: true,
+          video: true,
+          screen: false,
+        });
+        // Initialize local stream
+        localStream.current.init(() => {
+          console.log('Local stream initialized');
+          // Play local stream
+          localStream.current.play('local-stream');
+          // Publish local stream
+          client.current.publish(localStream.current, (err) => {
+            console.log('Publish local stream error: ' + err);
+          });
+        }, (err) => {
+          console.log('Local stream init error: ' + err);
+        });
+      }, (err) => {
+        console.log('Join channel error: ' + err);
+      });
+    }, (err) => {
+      console.log('AgoraRTC client init error: ' + err);
+    });
 
-    // Implement your video calling logic here using the Agora SDK
+    // Subscribe to remote stream
+    client.current.on('stream-added', (evt) => {
+      console.log('Remote stream added');
+      client.current.subscribe(evt.stream, (err) => {
+        console.log('Subscribe remote stream error: ' + err);
+      });
+    });
+    client.current.on('stream-subscribed', (evt) => {
+      console.log('Remote stream subscribed');
+      remoteStream.current = evt.stream;
+      remoteVideo.current.srcObject = remoteStream.current.stream;
+    });
+    client.current.on('stream-removed', (evt) => {
+      console.log('Remote stream removed');
+      if (remoteStream.current) {
+        remoteStream.current.stop();
+        remoteStream.current = null;
+      }
+      remoteVideo.current.srcObject = null;
+    });
 
-    // Remember to clean up the video call when the component unmounts
+    // Leave channel on component unmount
     return () => {
-      // Clean up the video call
+      client.current.leave(() => {
+        console.log('User left channel');
+        if (localStream.current) {
+          localStream.current.stop();
+          localStream.current.close();
+          localStream.current = null;
+        }
+        if (remoteStream.current) {
+          remoteStream.current.stop();
+          remoteStream.current.close();
+          remoteStream.current = null;
+        }
+      }, (err) => {
+        console.log('Leave channel error: ' + err);
+      });
     };
   }, []);
-  
+
   return (
     <div>
-      {/* Add your video call UI components here */}
-      <VideoCalling meetingId={meetingId} />
+      <div id="local-stream"></div>
+      <video ref={remoteVideo} autoPlay></video>
     </div>
   );
 };
 
 export default JoinMeeting;
-
--- app/video-calling/create-new-meeting/page.jsx
+++ app/video-calling/create-new-meeting/page.jsx
@@ -1,138 +1,70 @@
 "use client";
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Link from "next/link";
 import { useSession } from 'next-auth/react';
-// import { createClient,createMicrophoneAndCameraTracks,ClientConfig,ConnectionState } from 'agora-rtc-sdk-ng';
-import AgoraRTC from 'agora-rtc-sdk-ng';
 
-    const CreateMeetingButton = () => {
-        const [meetingLink, setMeetingLink] = useState('');
-        const { data: session } = useSession();
+const CreateMeetingButton = () => {
+    const [meetingLink, setMeetingLink] = useState('');
+    const { data: session } = useSession();
 
-        //   const createMeeting = async () => {
-        //     try {
-        //       const response = await fetch('/api/createMeeting');
-        //       const data = await response.json();
-        //       const { meetingLink } = data;
-        //       setMeetingLink(meetingLink);
-        //     } catch (error) {
-        //       console.error('Error creating meeting:', error);
-        //     }
-        //   };
+    //   const createMeeting = async () => {
+    //     try {
+    //       const response = await fetch('/api/createMeeting');
+    //       const data = await response.json();
+    //       const { meetingLink } = data;
+    //       setMeetingLink(meetingLink);
+    //     } catch (error) {
+    //       console.error('Error creating meeting:', error);
+    //     }
+    //   };
 
-        // const createMeeting = async () => {
-        //     try {
-                
-        //         const response = await fetch('/api/create-meeting', {
-        //             method: 'POST',
-        //             body: JSON.stringify({
-        //                 creator: session?.user.id,
-        //             })
-        //         });
-        
-        //         if (response.ok) {
-        //             const data = await response.json();
-        //             console.log('Response:', data); // Log the entire response data
-        //             const myMeetingLink = data.meeting_link;
-        //             setMeetingLink(myMeetingLink);
-        //             console.log('Meeting created successfully:', meetingLink);
-        //         } else {
-        //             console.error('Error creating meeting:', response.status);
-        //         }
-        //     } catch (error) {
-        //       console.error('Error creating meeting:', error);
-        //     }
-        // };
-        
-        // const createMeeting = async () => {
-        //     try {
-        //         const agoraAppId = 'b6b121e15e504a808e7092382509aab1';
-
-        //         const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-        //         await client.join(agoraAppId, 'MEDICAL_APP_7872307047', null, session?.user.id);
-
-        //         const cameraTrack = await AgoraRTC.createCameraVideoTrack();
-        //         const microphoneTrack = await AgoraRTC.createMicrophoneAudioTrack();
-
-        //         await client.publish([cameraTrack, microphoneTrack]);
-
-        //         client.on('user-published', async (user, mediaType) => {
-        //             await client.subscribe(user, mediaType);
-        //         });
-
-        //         cameraTrack.play('local-video-element-id');
-
-        //         client.on('user-subscribed', (user, mediaType) => {
-        //             const remoteVideoElement = document.createElement('video');
-        //             user.videoTrack.play(remoteVideoElement);
-        //             // Append remoteVideoElement to the DOM
-        //         });
-
-        //         client.on(ConnectionState.Disconnected, () => {
-        //         // Handle disconnection
-        //         });
-
-        //         // Clean up the resources when no longer needed
-        //         client.leave();
-        //         cameraTrack.stop();
-        //         microphoneTrack.stop();
-        //     } catch (error) {
-        //         console.log(`Error is : ${error}`);
-        //     }
-        // };
-
-        const joinMeeting = async (appId, channelName, userId) => {
-            try {
-                // Create an AgoraRTC client
-                const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-
-                // Initialize the client with your App ID
-                await client.init(appId);
-
-                // Join the channel with the specified channel name and user ID
-                await client.join(null, channelName, null, userId);
-
-                // Add your logic for handling the successful join here
-
-                // Clean up the resources when no longer needed
-                client.leave();
-            } catch (error) {
-                console.error('Error joining meeting:', error);
+    const createMeeting = async () => {
+        try {
+            
+            const response = await fetch('/api/create-meeting', {
+                method: 'POST',
+                body: JSON.stringify({
+                    creator: session?.user.id,
+                })
+            });
+    
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Response:', data); // Log the entire response data
+                const myMeetingLink = data.meeting_link;
+                setMeetingLink(myMeetingLink);
+                console.log('Meeting created successfully:', meetingLink);
+            } else {
+                console.error('Error creating meeting:', response.status);
             }
-        };
-
-        // Example usage
-        const appId = 'b6b121e15e504a808e7092382509aab1';
-        const channelName = 'MEDICAL_APP_7872307047';
-        const userId = session?.user.id;
-
-        
-        useEffect(() => {
-            joinMeeting(appId, channelName, userId);
-        }, []);
-
-        return (
-            <section className="w-full flex-center flex-col">
-                <h1 className="head_text text-center">
-                    Discober & Shear
-                    <br className="max-md:hidden"/>
-                    <span className="orange_gradient text-center"> AI-Powred Prompts </span>
-                    <p className="desc text-center">
-                    Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts 
-                    </p>
-
-                    <div className=''>
-                        {/* <button className='px-5 py-1.5 text-sm bg-orange-600 rounded-full text-white' onClick={createMeeting}>Create Meeting</button> */}
-                        {/* {meetingLink && <p className='font-santoshi font-semibold text-sm  pt-6 text-gray-500'>Meeting Link: 
-                            <Link href={`/video-calling/${meetingLink}`} className="flex gap-2 flex-center"> 
-                                {meetingLink} 
-                            </Link>
-                        </p>} */}
-                    </div>
-                </h1>
-            </section>
-        
-        );
+        } catch (error) {
+            console.error('Error creating meeting:', error);
+        }
     };
+    
+
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discober & Shear
+                <br className="max-md:hidden"/>
+                <span className="orange_gradient text-center"> AI-Powred Prompts </span>
+                <p className="desc text-center">
+                Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts 
+                </p>
+
+                <div className=''>
+                    <button className='px-5 py-1.5 text-sm bg-orange-600 rounded-full text-white' onClick={createMeeting}>Create Meeting</button>
+                    {meetingLink && <p className='font-santoshi font-semibold text-sm  pt-6 text-gray-500'>Meeting Link: 
+                        <Link href={`/video-calling/${meetingLink}`} className="flex gap-2 flex-center"> 
+                            {meetingLink} 
+                        </Link>
+                    </p>}
+                </div>
+            </h1>
+        </section>
+    
+    );
+};
 
 export default CreateMeetingButton;
