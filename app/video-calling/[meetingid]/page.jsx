44d8c80a1136f4e0237718d0484904170f0f005f
Subhodeep Bhattacharjee <hi.subho1994@gmail.com>
2023-07-19 09:22:30 +0530

    changes video calling

--- app/video-calling/[meetingid]/page.jsx
+++ app/video-calling/[meetingid]/page.jsx
@@ -82,99 +82,109 @@
 // export default JoinMeetingComponent;
 
 
-import React, { useEffect, useRef } from 'react';
-import AgoraRTC from 'agora-rtc-sdk';
+// import React, { useEffect, useRef } from 'react';
+// import AgoraRTC from 'agora-rtc-sdk';
+
+// const JoinMeeting = () => {
+//   const client = useRef(null);
+//   const localStream = useRef(null);
+//   const remoteStream = useRef(null);
+//   const remoteVideo = useRef(null);
+
+//   useEffect(() => {
+//     const appId = 'b6b121e15e504a808e7092382509aab1';
+//     const token = null; // Set to null if not using token
+//     const channelName = 'MedicalApp_307047';
+
+//     // Initialize Agora client
+//     client.current = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+//     client.current.init(appId, () => {
+//       console.log('AgoraRTC client initialized');
+//       client.current.join(token, channelName, null, (uid) => {
+//         console.log('User ' + uid + ' joined channel ' + channelName);
+//         // Create local stream
+//         localStream.current = AgoraRTC.createStream({
+//           streamID: uid,
+//           audio: true,
+//           video: true,
+//           screen: false,
+//         });
+//         // Initialize local stream
+//         localStream.current.init(() => {
+//           console.log('Local stream initialized');
+//           // Play local stream
+//           localStream.current.play('local-stream');
+//           // Publish local stream
+//           client.current.publish(localStream.current, (err) => {
+//             console.log('Publish local stream error: ' + err);
+//           });
+//         }, (err) => {
+//           console.log('Local stream init error: ' + err);
+//         });
+//       }, (err) => {
+//         console.log('Join channel error: ' + err);
+//       });
+//     }, (err) => {
+//       console.log('AgoraRTC client init error: ' + err);
+//     });
+
+//     // Subscribe to remote stream
+//     client.current.on('stream-added', (evt) => {
+//       console.log('Remote stream added');
+//       client.current.subscribe(evt.stream, (err) => {
+//         console.log('Subscribe remote stream error: ' + err);
+//       });
+//     });
+//     client.current.on('stream-subscribed', (evt) => {
+//       console.log('Remote stream subscribed');
+//       remoteStream.current = evt.stream;
+//       remoteVideo.current.srcObject = remoteStream.current.stream;
+//     });
+//     client.current.on('stream-removed', (evt) => {
+//       console.log('Remote stream removed');
+//       if (remoteStream.current) {
+//         remoteStream.current.stop();
+//         remoteStream.current = null;
+//       }
+//       remoteVideo.current.srcObject = null;
+//     });
+
+//     // Leave channel on component unmount
+//     return () => {
+//       client.current.leave(() => {
+//         console.log('User left channel');
+//         if (localStream.current) {
+//           localStream.current.stop();
+//           localStream.current.close();
+//           localStream.current = null;
+//         }
+//         if (remoteStream.current) {
+//           remoteStream.current.stop();
+//           remoteStream.current.close();
+//           remoteStream.current = null;
+//         }
+//       }, (err) => {
+//         console.log('Leave channel error: ' + err);
+//       });
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <div id="local-stream"></div>
+//       <video ref={remoteVideo} autoPlay></video>
+//     </div>
+//   );
+// };
+
+// export default JoinMeeting;
+
+import VideoCalling from "@components/VideoCalling";
 
 const JoinMeeting = () => {
-  const client = useRef(null);
-  const localStream = useRef(null);
-  const remoteStream = useRef(null);
-  const remoteVideo = useRef(null);
-
-  useEffect(() => {
-    const appId = 'b6b121e15e504a808e7092382509aab1';
-    const token = null; // Set to null if not using token
-    const channelName = 'MedicalApp_307047';
-
-    // Initialize Agora client
-    client.current = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-    client.current.init(appId, () => {
-      console.log('AgoraRTC client initialized');
-      client.current.join(token, channelName, null, (uid) => {
-        console.log('User ' + uid + ' joined channel ' + channelName);
-        // Create local stream
-        localStream.current = AgoraRTC.createStream({
-          streamID: uid,
-          audio: true,
-          video: true,
-          screen: false,
-        });
-        // Initialize local stream
-        localStream.current.init(() => {
-          console.log('Local stream initialized');
-          // Play local stream
-          localStream.current.play('local-stream');
-          // Publish local stream
-          client.current.publish(localStream.current, (err) => {
-            console.log('Publish local stream error: ' + err);
-          });
-        }, (err) => {
-          console.log('Local stream init error: ' + err);
-        });
-      }, (err) => {
-        console.log('Join channel error: ' + err);
-      });
-    }, (err) => {
-      console.log('AgoraRTC client init error: ' + err);
-    });
-
-    // Subscribe to remote stream
-    client.current.on('stream-added', (evt) => {
-      console.log('Remote stream added');
-      client.current.subscribe(evt.stream, (err) => {
-        console.log('Subscribe remote stream error: ' + err);
-      });
-    });
-    client.current.on('stream-subscribed', (evt) => {
-      console.log('Remote stream subscribed');
-      remoteStream.current = evt.stream;
-      remoteVideo.current.srcObject = remoteStream.current.stream;
-    });
-    client.current.on('stream-removed', (evt) => {
-      console.log('Remote stream removed');
-      if (remoteStream.current) {
-        remoteStream.current.stop();
-        remoteStream.current = null;
-      }
-      remoteVideo.current.srcObject = null;
-    });
-
-    // Leave channel on component unmount
-    return () => {
-      client.current.leave(() => {
-        console.log('User left channel');
-        if (localStream.current) {
-          localStream.current.stop();
-          localStream.current.close();
-          localStream.current = null;
-        }
-        if (remoteStream.current) {
-          remoteStream.current.stop();
-          remoteStream.current.close();
-          remoteStream.current = null;
-        }
-      }, (err) => {
-        console.log('Leave channel error: ' + err);
-      });
-    };
-  }, []);
-
   return (
-    <div>
-      <div id="local-stream"></div>
-      <video ref={remoteVideo} autoPlay></video>
-    </div>
-  );
-};
+    <VideoCalling />
+  )
+}
 
 export default JoinMeeting;
--- components/VideoCalling.jsx
+++ components/VideoCalling.jsx
@@ -1,71 +1,111 @@
 "use client";
+// import { useEffect } from 'react';
+// import AgoraRTC from 'agora-rtc-sdk';
+
+// const VideoCalling = ({ meetingId }) => {
+//   useEffect(() => {
+//     const agoraAppId = 'b6b121e15e504a808e7092382509aab1';
+//     const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+//     console.log(agoraAppId);
+//     console.log(`AgoraRTC-createClient -> ${client}`);
+//     let localStream = null;
+
+//     const joinMeeting = async () => {
+//       try {
+//         console.log('joinMeeting', agoraAppId);
+//         // Initialize the Agora client
+//         await client.init(agoraAppId);
+
+//         console.log(`client-init-agoraAppId- -> ${client}`);
+
+//         // Join the channel using the meetingId as the channel name
+//         await client.join('HRMS meeting', meetingId, null, (uid) => {
+//           // Create and initialize the local stream
+//           localStream = AgoraRTC.createStream({
+//             streamID: uid,
+//             audio: true,
+//             video: true,
+//           });
+
+//         console.log(`AgoraRTC-|createStream|- -> ${client}`);
+
+//           localStream.init(() => {
+//             // Play the local stream
+
+//             // Replace 'local-video' with the ID or ref of your local video element
+//             localStream.play('local-video');
+
+//             // Publish the local stream to the channel
+//             client.publish(localStream);
+//           });
+//         });
+//       } catch (error) {
+//         console.error('Error joining meeting:', error);
+//       }
+//     };
+
+//     // Call the joinMeeting function to join the meeting
+//     joinMeeting();
+
+//     // Clean up the video call when the component unmounts
+//     return () => {
+//       if (localStream) {
+//         localStream.stop();
+//         localStream.close();
+//       }
+//       if (client) {
+//         client.leave();
+//       }
+//     };
+//   }, [meetingId]);
+
+//   return (
+//     <div>
+//       <h1>Join Video Call</h1>
+//       <div id="local-video"></div>
+//       {/* Add your video call UI components here */}
+//     </div>
+//   );
+// };
+
+// export default VideoCalling;
+
 import { useEffect } from 'react';
-import AgoraRTC from 'agora-rtc-sdk';
+import AgoraRTC from 'agora-rtc-sdk-ng';
 
-const VideoCalling = ({ meetingId }) => {
+function VideoCalling() {
   useEffect(() => {
-    const agoraAppId = 'b6b121e15e504a808e7092382509aab1';
+    // Initialize AgoraRTC client
     const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-    console.log(agoraAppId);
-    console.log(`AgoraRTC-createClient -> ${client}`);
-    let localStream = null;
 
-    const joinMeeting = async () => {
-      try {
-        console.log('joinMeeting', agoraAppId);
-        // Initialize the Agora client
-        await client.init(agoraAppId);
+    // Join a channel
+    client.join('b6b121e15e504a808e7092382509aab1', 'learncuremedicalaap', null, null);
 
-        console.log(`client-init-agoraAppId- -> ${client}`);
-
-        // Join the channel using the meetingId as the channel name
-        await client.join('HRMS meeting', meetingId, null, (uid) => {
-          // Create and initialize the local stream
-          localStream = AgoraRTC.createStream({
-            streamID: uid,
-            audio: true,
-            video: true,
-          });
-
-        console.log(`AgoraRTC-|createStream|- -> ${client}`);
-
-          localStream.init(() => {
-            // Play the local stream
-
-            // Replace 'local-video' with the ID or ref of your local video element
-            localStream.play('local-video');
-
-            // Publish the local stream to the channel
-            client.publish(localStream);
-          });
-        });
-      } catch (error) {
-        console.error('Error joining meeting:', error);
-      }
-    };
-
-    // Call the joinMeeting function to join the meeting
-    joinMeeting();
-
-    // Clean up the video call when the component unmounts
-    return () => {
-      if (localStream) {
-        localStream.stop();
-        localStream.close();
-      }
-      if (client) {
-        client.leave();
-      }
-    };
-  }, [meetingId]);
+    // Rest of the Agora Video Calling implementation
+  }, []);
 
   return (
     <div>
-      <h1>Join Video Call</h1>
-      <div id="local-video"></div>
-      {/* Add your video call UI components here */}
+      <div id="localVideoContainer">
+        {/* Display the local video stream */}
+        <video id="localVideo" autoPlay muted></video>
+      </div>
+
+      <div id="remoteVideoContainer">
+        {/* Display the remote video streams */}
+        <video id="remoteVideo1" autoPlay></video>
+        <video id="remoteVideo2" autoPlay></video>
+      </div>
+
+      <div id="controls" className='flex flex-center'>
+        {/* Buttons and controls for video calling functionality */}
+        <button class="outline_btn" id="joinButton">Join Call</button>
+        <button class="px-5 py-1.5 text-sm bg-red-600 rounded-full text-white" id="leaveButton">Leave Call</button>
+        <button class="outline_btn" id="muteButton">Toggle Mute</button>
+        <button class="outline_btn" id="shareScreenButton">Share Screen</button>
+      </div>
     </div>
-  );
-};
+  );  
+}
 
 export default VideoCalling;
